e190f222f1ea2460f6401813fef00a0feef945f2
Aliaksandra Hurskaya <sandrahurskaya@gmail.com>
2024-04-18 14:58:44 +0200

    Add rating stars

--- client/src/components/movieContainer/movieContainer.jsx
+++ client/src/components/movieContainer/movieContainer.jsx
@@ -1,3 +1,4 @@
+import ReactStars from "react-rating-stars-component";
 import "./movieContainer.scss";
 
 // eslint-disable-next-line react/prop-types
@@ -6,13 +7,22 @@ const MovieContainer = ({ movieInformation }) => {
     const { wasViewed, poster_path, title, rating } = movieInformation;
     const img_path = "https://image.tmdb.org/t/p/original";
 
+    const firstExample = {
+        size: 27,
+        value: rating / 2,
+        edit: false,
+        activeColor: "#9f0013",
+    };
+
     return (
         <div className="movie">
             <div className={wasViewed ? "movie_wasViewed" : null}></div>
             <img src={`${img_path}${poster_path}`} alt="" />
             <div className="movie_information">
                 <p>{title}</p>
-                <p>{rating}</p>
+            </div>
+            <div className="movie_rating-stars">
+                <ReactStars {...firstExample} />
             </div>
         </div>
     );
